26fec3a7839cc90177b68657c1263a33c8d87beb
Sherman Adelson <sherman.adelson@gmail.com>
2015-07-25 17:37:56 -0400

    changed loading logic to use path module

--- app-uses/index.js
+++ app-uses/index.js
@@ -2,15 +2,16 @@
 Export a function to initialize all the app.use calls
 */
 var fs = require('fs');
+var path = require('path');
 
 var requires = [];
 
 fs.readdirSync(__dirname).forEach(function(filename) {
-	//TODO: regex for matching file names
 	// We probably don't want names starting with .
 	// We want to limit it to names ending with .js
-	if (filename !== 'index.js' && filename.indexOf('.js') !== -1) {
-		var name = filename.substr(0, filename.indexOf('.js'));
+	var ext = path.extname(filename);
+	var name = path.basename(filename,ext);
+	if( filename !== 'index.js' && '.js'===ext ){
 		var func = require('./' + name);
 		requires.push(func);
 	}
--- routes/index.js
+++ routes/index.js
@@ -5,14 +5,15 @@ Requires each route file, which exports a function.
 The exported function takes app as an argument and registers all routes.
 */
 var fs = require('fs');
+var path = require('path');
 
 function setupRoutes(app) {
 	fs.readdirSync(__dirname).forEach(function(filename) {
-		//TODO: regex for matching file names
 		// We probably don't want names starting with .
 		// We want the name to end with .js
-		if (filename !== 'index.js' && filename.indexOf('.js') !== -1) {
-			var name = filename.substr(0, filename.indexOf('.js'));
+		var ext = path.extname(filename);
+		var name = path.basename(filename,ext);
+		if( filename !== 'index.js' && '.js'===ext ){
 			require('./' + name)(app);
 		}
 	});
