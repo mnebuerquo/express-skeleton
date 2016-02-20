6d48fa104616d9d16b5ff1eba7f17a335b7df2f7
Mnebuerquo <sherman.adelson@gmail.com>
2016-02-20 15:21:09 -0500

    fixed debug so I can turn on break at start

--- debug.sh
+++ debug.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
-if [ $1 = "no-debug" ]; then
+if [ "$1" = "no-debug" ] 
+then
 	echo "Not using debugger!"
 	COMMAND="node"
 	OPTION=""
 else
 	echo "Debugging app."
 	COMMAND="node-debug"
-	OPTION="--debug-brk=0 -p 7000"
+	BRK=""
+	if [ "$1" != "break" ]; then
+		BRK="--debug-brk=0"
+	fi
+	OPTION="$BRK -p 7000"
 fi
 
 CMD="NODE_ENV=development $COMMAND $OPTION app.js"
